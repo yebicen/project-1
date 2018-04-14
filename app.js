4a5c72ca9f360c7ed05088401cc88946b31a950c
Levan <l.dvalidze@gmail.com>
2018-04-14 13:18:45 -0700

    firebase initielized

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBQKVvElJCzBRpp49UB1kXZVfnM_ty9fOU",
+        authDomain: "project-1-97352.firebaseapp.com",
+        databaseURL: "https://project-1-97352.firebaseio.com",
+        projectId: "project-1-97352",
+        storageBucket: "project-1-97352.appspot.com",
+        messagingSenderId: "711415812320"
+    };
+    firebase.initializeApp(config);
+
+    
+})
