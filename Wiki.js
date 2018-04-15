c57844b70b8d5d41e59bdab6be4a2c1e2cfdb6b8
Bicen Ye <bicenye@Bicens-MacBook-Pro.local>
2018-04-15 13:31:36 -0700

    updated

--- Wiki.js
+++ Wiki.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     function displayWikipedia() {
         $("#wiki").empty();
-        
+
         var searchID = $(this).attr("data-name");
         console.log(searchID);
 
@@ -21,14 +21,21 @@ $(document).ready(function(){
             var newPanel = $("<div>").attr("class","panel panel-primary");
             var newPanelHeading = $("<div>").attr("class","panel-heading");
             var newPanelbody = $("<div>").attr("class","panel-body");
+            var externalWikiLink = $("<div>").attr("class","alert alert-secondary");
 
-            var Title = $("<h3>").text(response.title);
-            var pOne = $("<p><strong>").text(response.description);
-            var pTwo = $("<p>").text(response.extract);
-            var pThree = $("<p>").text(response.api_urls.summary);
+            console.log(response.content_urls.desktop.page)
 
+            var pThree = "<a href="  + response.content_urls.desktop.page + " target='_blank'>Check more on Wikipedia</a>";
+            $(pThree).attr("class","alert-link");
          
 
+            var pFour = "<a href= https://en.wikipedia.org/api/rest_v1/page/pdf/"  + searchID + ">Download PDF</a>";
+            $(pFour).attr("class","alert-link")
+
+            var Title = $("<h3>").text(response.title);
+            var pOne = $("<h4>").text(response.description);
+            var pTwo = $("<p>").text(response.extract);
+        
             $("#wiki").append(newPanelGroup)
             newPanelGroup.append(newPanel);
             newPanel.append(newPanelHeading);
@@ -37,7 +44,9 @@ $(document).ready(function(){
 
            newPanelbody.append(pOne);
            newPanelbody.append(pTwo);
-           newPanelbody.append(pThree);
+           newPanelbody.append(externalWikiLink);
+           externalWikiLink.append(pThree);
+           externalWikiLink.append(pFour);
         
          });
 
