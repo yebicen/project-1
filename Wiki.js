df300bcd73c99677b6feaf1bed292173a90adee0
Luqiang Mao <luqiangmao@Luqiangs-MacBook-Pro.local>
2018-04-17 18:51:17 -0700

    fix empty div

--- Wiki.js
+++ Wiki.js
@@ -68,9 +68,12 @@ $(document).ready(function(){
        }).then(function(response) {
          //console.log(response.docs[1].title);
          //console.log(JSON.parse(response).docs[0]);
-         //console.log(JSON.parse(response).docs.length);
+         console.log(JSON.parse(response).docs.length);
+
+
+         //need to test min(4,JSON.parse(response).docs.length)
          var n = 0;
-         for (var i = 0; i < 4; i++) {
+         for (var i = 0; i < min(4,JSON.parse(response).docs.length); i++) {
              n=i+1;   
              $('<div>').attr({"id":"book"+n,"class":"book"}).appendTo("#suggestedBooks");
              //console.log("i: "+ i+ " n: "+n);
@@ -131,51 +134,51 @@ $(document).ready(function(){
              //assign href to buy button
  
      //custom bing search api
-    //  var docType = "pdf";
-    //  var onlineSearch = topic + " " + docType;
-    //  var params2 = {
-    //    // Request parameters
-    //      "q": onlineSearch,
-    //      "customconfig": customConfigId,
-    //      "mkt": "en-US",
-    //      "safe-search": "Moderate",
-    //      count: 5,
-    //      offset: 0
+     var docType = "pdf";
+     var onlineSearch = topic + " " + docType;
+     var params2 = {
+       // Request parameters
+         "q": onlineSearch,
+         "customconfig": customConfigId,
+         "mkt": "en-US",
+         "safe-search": "Moderate",
+         count: 5,
+         offset: 0
      
-    //   };
+      };
       
-    //  $.ajax({
-    //    url: "https://api.cognitive.microsoft.com/bingcustomsearch/v7.0/search?" + $.param(params2),
-    //    beforeSend: function(xhrObj){
-    //        // Request headers
-    //       xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", apikey);
-    //    },
-    //    type: "GET",
-    //    // Request body 
-    //    data: "",
-    //  })
-    //  .done(function(data) {
-    //    //alert("success");
-    //    console.log(data);
-    //    var y=0;
-    //    for (var i = 0; i < 5; i++) {
-    //      y=i+1;   
-    //      //console.log("i: "+ i+ " y: "+y);
+     $.ajax({
+       url: "https://api.cognitive.microsoft.com/bingcustomsearch/v7.0/search?" + $.param(params2),
+       beforeSend: function(xhrObj){
+           // Request headers
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", apikey);
+       },
+       type: "GET",
+       // Request body 
+       data: "",
+     })
+     .done(function(data) {
+       //alert("success");
+       console.log(data);
+       var y=0;
+       for (var i = 0; i < 5; i++) {
+         y=i+1;   
+         //console.log("i: "+ i+ " y: "+y);
            
-    //      $("<div>").attr("id","doc"+y).appendTo("#onlineResources");
-    //      $("<div>").text("Description: "+data.webPages.value[i].snippet).attr("class", "docDesc").prependTo("#doc"+y);   
-    //      $("<div>").text("Name: "+data.webPages.value[i].name).attr("class", "docName").prependTo("#doc"+y); 
+         $("<div>").attr("id","doc"+y).appendTo("#onlineResources");
+         $("<div>").text("Description: "+data.webPages.value[i].snippet).attr("class", "docDesc").prependTo("#doc"+y);   
+         $("<div>").text("Name: "+data.webPages.value[i].name).attr("class", "docName").prependTo("#doc"+y); 
         
-    //      $('<a role="button" class="btn btn-primary">').attr("id","download"+y).text("Download").appendTo("#doc"+y);
-    //      $('<button type="button" class="btn btn-dark">').text("Save").appendTo("#doc"+y);
-    //      $(("#download"+y)).attr("href",data.webPages.value[i].url);
-    //      $('<hr>').appendTo("#doc"+y);
+         $('<a role="button" class="btn btn-primary">').attr("id","download"+y).text("Download").appendTo("#doc"+y);
+         $('<button type="button" class="btn btn-dark save">').text("Save").appendTo("#doc"+y);
+         $(("#download"+y)).attr("href",data.webPages.value[i].url);
+         $('<hr>').appendTo("#doc"+y);
                                    
-    //      }
-    //  })
-    //  .fail(function() {
-    //    console.log("error");
-    //  });
+         }
+     })
+     .fail(function() {
+       console.log("error");
+     });
  
  
  
@@ -183,7 +186,10 @@ $(document).ready(function(){
 
     //onclick the term items    
     $(".term").on("click", displayWikipedia);
-   
+     
+    $('#bookList').on("click", 'button[class=save]',function(){
+
+    });
 //end of ducoment ready	
 });
     
